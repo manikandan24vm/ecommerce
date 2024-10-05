9a9e1e192676fe58722f867833ee3f92160453e4
manikandan24vm <manikandan24vm@gmail.com>
2024-10-05 22:36:05 +0530

    changes updated-[05-10-2024/SAT-10:35pm]

--- SpringBootMicroservices/user-service/src/main/java/com/ecommerce/user_service/dto/AddressDTO.java
+++ SpringBootMicroservices/user-service/src/main/java/com/ecommerce/user_service/dto/AddressDTO.java
@@ -3,6 +3,8 @@ package com.ecommerce.user_service.dto;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +13,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AddressDTO {
-
     public String addressLine1;
     public String addressLine2;
     public String addressLine3;
+    @NotEmpty
+    @Pattern(regexp = "(^$|[0-9]{6})",message = "invalid pin code")
     public String pin;
 }
--- SpringBootMicroservices/user-service/src/main/java/com/ecommerce/user_service/service/impl/UserDetailsServiceImpl.java
+++ SpringBootMicroservices/user-service/src/main/java/com/ecommerce/user_service/service/impl/UserDetailsServiceImpl.java
@@ -1,12 +1,15 @@
 package com.ecommerce.user_service.service.impl;
 
 import com.ecommerce.user_service.entity.Users;
+import com.ecommerce.user_service.exception.UserNotFoundException;
 import com.ecommerce.user_service.repository.UserServiceRepository;
 import com.ecommerce.user_service.service.UserService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
@@ -14,15 +17,26 @@ import java.util.List;
 
 
 @Service
-@AllArgsConstructor
+
 public class UserDetailsServiceImpl implements UserService {
     private static final Logger log = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+    @Autowired
     private UserServiceRepository userServiceRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public Users createUser(Users user) {
-        log.info("saving user...");
-        return userServiceRepository.save(user);
+        try {
+            log.info("saving user...");
+            String password = user.getPassword();
+            user.setPassword(passwordEncoder.encode(password));
+            return userServiceRepository.save(user);
+        }
+        catch (Exception e){
+          throw  new RuntimeException("cannot create the user.."+e);
+        }
+
     }
 
     @Override
@@ -31,13 +45,13 @@ public class UserDetailsServiceImpl implements UserService {
         if (!CollectionUtils.isEmpty(usersList)) {
             return usersList;
         } else {
-            throw new RuntimeException("no users found");
+            throw new UserNotFoundException("no users found");
         }
     }
 
     @Override
     public Users updateUser(Long userId, Users user) {
-        Users userData = userServiceRepository.findById(userId).orElseThrow(() -> new RuntimeException("user not found"));
+        Users userData = userServiceRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("user not found"));
         userData.setFirstName(user.getFirstName());
         userData.setLastName(user.getLastName());
         userData.setEmail(user.getEmail());
@@ -50,12 +64,12 @@ public class UserDetailsServiceImpl implements UserService {
 
     @Override
     public Users getUserById(Long userId) {
-        return userServiceRepository.findById(userId).orElseThrow(() -> new RuntimeException("user not found with a ID :" + userId));
+        return userServiceRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("user not found with a ID :" + userId));
     }
 
     @Override
     public void deleteUserById(Long userId) {
-        Users users = userServiceRepository.findById(userId).orElseThrow(() -> new RuntimeException("user not found with a ID :" + userId));
+        Users users = userServiceRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("user not found with a ID :" + userId));
         if (users != null) {
             userServiceRepository.deleteById(userId);
         }
