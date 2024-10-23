a6f3a2e6cbb3bcc4f07fc17e6b01d8518a488e14
manikandan24vm <manikandan24vm@gmail.com>
2024-10-23 20:39:10 +0530

    CHGS-23102024-Added liquid-base

--- SpringBootMicroservices/user-service/src/main/resources/sql/data.sql
+++ SpringBootMicroservices/user-service/src/main/resources/sql/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO user_db (user_id, first_name, last_name, password, email, phone_number, address_line1, address_line2, address_line3)
-       VALUES (100, 'Default', 'user', '{bcrypt}$2a$10$EZYtZ/beE61i1lZ7WdETNeBqk.G5bEDT0QUr5srGLzQ66yCxAp69u', 'default@mail.com', '1234567890','test','test','test');
+       VALUES (100, 'Default', 'user', '{bcrypt}$2a$12$8HH4cXoQ29ynPLKrdU2W9uwfrgPGrWsSm3DNkrQwy/pfOSm7KNE5O', 'default@mail.com', '1234567890','test','test','test');
 
 
 INSERT INTO role(role_id, user_id, description, role_names)
